cbb320378fad61772714c0e82b7d89a789966c84
NachoJusticia <nacho@graphext.com>
2018-10-11 17:48:54 +0200

    Use the 6th line of the css files to write the package.json version

--- packages/node-build-scripts/sass-compile.sh
+++ packages/node-build-scripts/sass-compile.sh
@@ -21,4 +21,4 @@ $ROOT_NM/.bin/node-sass-chokidar \
 PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g')
 
 # Add the package version to the header of the css output file
-sed -i '' "3s/^/Version: ${PACKAGE_VERSION}\ /" $OUTPUT*.css
+sed -i '' "6s/^/Version: ${PACKAGE_VERSION}\ /" $OUTPUT*.css
