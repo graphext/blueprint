b2b32962d37abd6de19896f6f150032e2eac99d0
NachoJusticia <nacho@graphext.com>
2018-10-17 12:55:13 +0200

    Add script generate-graphext-css

--- packages/node-build-scripts/sass-compile.sh
+++ packages/node-build-scripts/sass-compile.sh
@@ -16,9 +16,3 @@ $ROOT_NM/.bin/node-sass-chokidar \
   --output $OUTPUT \
   --source-map true \
   $@
-
-# Read the package.json version property
-PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g')
-
-# Add the package version to the header of the css output file
-sed -i '' "6s/^/Version: ${PACKAGE_VERSION}\ /" $OUTPUT*.css
