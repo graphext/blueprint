f58e469f13ff284d03cb90b2b9d861bf6f1bf7be
Luna Fidalgo <lunafidalgo@gmail.com>
2022-06-17 11:52:45 +0200

    style(Tabs): add class to disable hover effect

--- packages/core/src/common/classes.ts
+++ packages/core/src/common/classes.ts
@@ -386,3 +386,6 @@ export const TAB_STROKE_LINE_TOP = `${NS}-g-stroke-line-top`;
 export const TAB_STROKE_LINE_RIGHT = `${NS}-g-stroke-line-right`;
 export const TAB_STROKE_LINE_BOTTOM = `${NS}-g-stroke-line-bottom`;
 export const TAB_STROKE_LINE_LEFT = `${NS}-g-stroke-line-left`;
+
+/* TAB */
+export const TAB_DISABLE_HOVER_EFFECT = `${NS}-g-tab-disable-hover-effect`;
