b8c4357dd26f693d1b615a0f03087d1a4151748d
lunafidalgo <lunafidalgo@gmail.com>
2023-06-09 12:38:53 +0200

    improve name

--- packages/core/src/common/classes.ts
+++ packages/core/src/common/classes.ts
@@ -431,4 +431,4 @@ export const TAG_TOGGLE = `${NS}-g-toggle-tag`;
 export const LOZENGE = `${NS}-g-lozenge`;
 
 /* TABLE */
-export const TABLE_WITH_SAME_COLOR_AND_GRID = `${NS}-g-table-with-same-color-and-grid`;
+export const TABLE_WITH_SAME_BG_COLOR_AND_GRID = `${NS}-g-table-with-same-bg-color-and-grid`;
