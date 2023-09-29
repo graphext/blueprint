3dd351cf343178eb79f3f783a71a5dd541b1ff92
lunafidalgo <lunafidalgo@gmail.com>
2023-09-29 12:12:45 +0200

    fix styles that were broken/undesired in the new version

--- packages/core/src/common/classes.ts
+++ packages/core/src/common/classes.ts
@@ -450,7 +450,7 @@ export const TAB_STROKE_LINE_RIGHT = `${NS}-g-stroke-line-right`;
 export const TAB_STROKE_LINE_BOTTOM = `${NS}-g-stroke-line-bottom`;
 export const TAB_STROKE_LINE_LEFT = `${NS}-g-stroke-line-left`;
 
-export const TABS_FILL_SPACE = `${NS}-tabs-fill`;
+export const TABS_FILL_WIDTH = `${NS}-g-tabs-fill-width`;
 
 /* TAB */
 export const TAB_DISABLE_HOVER_EFFECT = `${NS}-g-tab-disable-hover-effect`;
