73f85b98800f3922c0b9b08770edad7f7dc3645d
Luna Fidalgo <36133677+LunaFidalgo@users.noreply.github.com>
2025-01-07 08:54:44 +0100

    feat(EditableText): add new small size (#122)

--- packages/core/src/common/classes.ts
+++ packages/core/src/common/classes.ts
@@ -471,3 +471,6 @@ export const LOZENGE = `${NS}-g-lozenge`;
 
 /* TABLE */
 export const TABLE_WITH_SAME_BG_COLOR_AND_GRID = `${NS}-g-table-with-same-bg-color-and-grid`;
+
+/* EDITABLE TEXT */
+export const EDITABLE_TEXT_SMALL = `${NS}-g-editable-text-small`;
